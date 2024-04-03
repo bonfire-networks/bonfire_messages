fc8e3f30646f47195a1e72da7669e5ef036a80d8
Mayel de Borniol <git@mayel.space>
2024-04-03 14:06:58 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/integration.ex
+++ lib/integration.ex
@@ -5,6 +5,12 @@ defmodule Bonfire.Messages.Integration do
   # alias Bonfire.Data.Social.Follow
   import Untangle
 
+  declare_extension("Messages",
+    icon: "carbon:email",
+    emoji: "✉️",
+    description: l("Functionality for writing and reading private messages.")
+  )
+
   def repo, do: Config.repo()
 
   def mailer, do: Config.get!(:mailer_module)
