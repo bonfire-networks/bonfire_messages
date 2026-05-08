38fcdb5821810c6cc8f590cbd7ecc80018a32d35
Mayel de Borniol <git@mayel.space>
2026-05-08 11:13:04 +0100

    up

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Bonfire.Messages.MixProject do
           {:bonfire_api_graphql,
            git: "https://github.com/bonfire-networks/bonfire_api_graphql",
            optional: true, runtime: false},
-          {:absinthe, "~> 1.7", optional: true}
+          {:absinthe, "~> 1.10", optional: true}
         ])
     ]
   end
